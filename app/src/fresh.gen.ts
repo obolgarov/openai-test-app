59f3c12d02ff6616ce3a287770c2f9e7ab0ba903
Oleksandr Bolgarov <obolgarov@Oleksandrs-MacBook-Pro.local>
2024-10-07 13:33:24 -0400

    monorepo workspaces not working well

--- app/src/fresh.gen.ts
+++ app/src/fresh.gen.ts
@@ -7,7 +7,7 @@ import * as $_app from "./routes/_app.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Countdown from "./islands/Countdown.tsx";
 import * as $Counter from "./islands/Counter.tsx";
-import { type Manifest } from "$fresh/server.ts";
+import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
