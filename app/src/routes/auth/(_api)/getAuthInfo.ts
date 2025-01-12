aba4c1eef6826094fde1536dd35cccb8d3b32c4a
Oleksandr Bolgarov <oleksandr@bolgarov.org>
2025-01-12 18:24:51 -0500

    using type in client

--- app/src/routes/auth/(_api)/getAuthInfo.ts
+++ app/src/routes/auth/(_api)/getAuthInfo.ts
@@ -1,17 +1,19 @@
 import environment from "#config/environment.ts";
 import { paths } from "#types/api-types.d.ts";
 
-const x: paths["/auth/info"]["get"];
+type AuthInfoResponse =
+  paths["/auth/info"]["get"]["responses"]["200"]["content"]["json"];
 
 export const getAuthInfo = async () => {
   const apiRoot = environment.app.api_url;
 
   const authInfoUrl = `${apiRoot}/auth/info`;
+
   const response = await fetch(authInfoUrl, {
     method: "GET",
   });
 
   const data = await response.json();
 
-  return data as getAuthInfoData;
+  return data as AuthInfoResponse;
 };
